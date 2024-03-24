5f30df4c9730710fa515797336ade13b94386f21
Mahmoud Abed <mahmoudabed120@gmail.com>
2024-03-24 04:32:26 +0200

    Add login page UI elements and update pubspec.yaml
    - Add login page UI elements including logo, text fields, and login button

--- lib/views/LoginPage.dart
+++ lib/views/LoginPage.dart
@@ -7,8 +7,40 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          
+          Image.asset(
+            "assets/images/logo.png",
+            fit: BoxFit.fill,
+          ),
+          Text("Login", style: TextStyle(fontSize: 30)),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Email",
+                hintText: "Enter Email",
+                prefixIcon: Icon(Icons.email),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.amberAccent)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.green)),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              "Login",
+              style: TextStyle(fontSize: 20, color: Colors.black),
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red, // <-- Background color
+            ),
+          )
         ],
       ),
     );
